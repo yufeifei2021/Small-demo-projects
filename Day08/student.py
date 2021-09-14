0543a4ed57ebb0cd9fbcc62859bb16c8848cad77
陈雨 <chenyu_13820592380@126.com>
2021-09-14 10:36:42 +0800

    面向对象编程基础

---
+++ Day08/student.py
@@ -0,0 +1,38 @@
+"""
+定义和使用学生类
+"""
+
+
+def _foo():
+    print('test')
+
+
+class Student(object):
+
+    # __init__是一个特殊方法用于在创建对象时进行初始化操作
+    # 通过这个方法我们可以为学生对象绑定name和age两个属性
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def study(self, course_name):
+        print('%s正在学习%s.' % (self.name, course_name))
+
+    def watch_av(self):
+        if self.age < 18:
+            print('%s只能观看《熊出没》.' % self.name)
+        else:
+            print('%s正在观看岛国大电影.' % self.name)
+
+
+def main():
+    stu1 = Student('骆昊', 38)
+    stu1.study('Python程序设计')
+    stu1.watch_av()
+    stu2 = Student('王大锤', 15)
+    stu2.study('思想品德')
+    stu2.watch_av()
+
+
+if __name__ == '__main__':
+    main()
