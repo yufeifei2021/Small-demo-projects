f8eeca4869e2bd99f0b587fd3377723716eefe3a
陈雨 <chenyu_13820592380@126.com>
2021-09-22 20:56:52 +0800

    图像和办公文档处理

---
+++ Day15/pdf2.py
@@ -0,0 +1,14 @@
+"""
+读取PDF文件
+"""
+
+from PyPDF2 import PdfFileReader
+
+with open(r'./res/Docker入门教程.pdf', 'rb+') as f:
+    reader = PdfFileReader(f, strict=False)
+    print(reader.numPages)
+    if reader.isEncrypted:
+        reader.decrypt('')
+    current_page = reader.getPage(5)
+    print(current_page)
+    print(current_page.extractText())
