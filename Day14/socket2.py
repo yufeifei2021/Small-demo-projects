2aa9877a6865ee443d7d4e0e6b55a4dc567a9526
陈雨 <chenyu_13820592380@126.com>
2021-09-22 09:16:51 +0800

    网络编程入门

---
+++ Day14/socket2.py
@@ -0,0 +1,14 @@
+"""
+套接字 - 基于TCP协议创建时间客户端
+"""
+
+from socket import *
+
+client = socket(AF_INET, SOCK_STREAM)
+client.connect(('localhost', 6789))
+while True:
+    data = client.recv(1024)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+client.close()
---
+++ Day14/socket3.py
@@ -0,0 +1,12 @@
+"""
+套接字 - 基于UDP协议Echo服务器
+"""
+from socket import *
+from time import *
+
+server = socket(AF_INET, SOCK_DGRAM)
+server.bind(('localhost', 6789))
+while True:
+    data, addr = server.recvfrom(1024)
+    server.sendto(data, addr)
+server.close()
